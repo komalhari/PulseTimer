eb53ca9242a98ada7a5c018c639bedf1934b44ff
komalhari7 <komalhari.dev@gmail.com>
2025-07-03 22:00:02 +0545

    signin/up form layout update

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -14,8 +14,8 @@ export default async function LoginPage() {
   }
 
   return (
-    <div className="bg-muted flex max-h-svh flex-col items-center justify-center p-6 md:p-10">
-      <div className="w-screen min-h-svh  sm:w-1/3 p-3 md:max-w-3xl">
+  <div className="bg-muted flex max-h-svh flex-col items-center justify-center p-6 md:p-10">
+    <div className="w-screen min-h-svh sm:w-1/3 p-3 md:max-w-3xl flex items-center justify-center">
         <LoginForm />
       </div>
     </div>
--- src/app/(auth)/signup/page.jsx
+++ src/app/(auth)/signup/page.jsx
@@ -5,7 +5,7 @@ export default async function SignUpPage() {
 //
   return (
    <div className="bg-muted flex max-h-svh flex-col items-center justify-center p-6 md:p-10">
-      <div className="w-screen min-h-svh sm:w-1/3 p-3 md:max-w-3xl ">
+      <div className="w-screen min-h-svh sm:w-1/3 p-3 md:max-w-3xl flex items-center justify-center">
         <SignUpForm />
       </div>
     </div>
